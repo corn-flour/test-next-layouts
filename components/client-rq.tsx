169b047c806b8fba680362367bb9eca3b07c3fa2
Huy Anh Nguyen <26082352+bte234@users.noreply.github.com>
2022-10-26 15:11:51 -0700

    test with pokeapi

--- components/client-rq.tsx
+++ components/client-rq.tsx
@@ -4,10 +4,12 @@ import { useQuery } from "@tanstack/react-query";
 import { getBaseUrl } from "../utils/get-base-url";
 
 const fetchData = async () =>
-  fetch(`${getBaseUrl()}/api/hello`).then((res) => res.json());
+  fetch("https://pokeapi.co/api/v2/pokemon/bulbasaur").then((res) =>
+    res.json()
+  );
 
 export const RQFetch = () => {
-  console.log(`test RQ: ${getBaseUrl()}/api/hello`);
+  console.log(`test RQ: https://pokeapi.co/api/v2/pokemon/bulbasaur`);
   const { data } = useQuery(["fetchdata"], fetchData);
 
   if (!data) return <div>RQ loading...</div>;
