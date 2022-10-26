1dc38b3b2b1505ac106b0e763564815af4c337fa
Huy Anh Nguyen <26082352+bte234@users.noreply.github.com>
2022-10-25 19:51:38 -0700

    add some explain paragraphs

--- app/page.tsx
+++ app/page.tsx
@@ -10,6 +10,9 @@ export default function Home() {
 
       <section>
         <h2 className="text-2xl font-bold">Server API with async</h2>
+        <p className="text-sm text-gray-600">
+          This should become static on build
+        </p>
         {/**
          * This component breaks TS because it is not expecting an async component yet
          * @ts-ignore */}
@@ -18,17 +21,35 @@ export default function Home() {
 
       <section>
         <h2 className="text-2xl font-bold">Server API with use()</h2>
+        <p className="text-sm text-gray-600">
+          As far as I can tell from the documentation, use() should only be for
+          frontend, but this somehow still works? It has the exact same result
+          as using async server component as far as I can tell (aside from the
+          lack of TS error).
+        </p>
         <ServerContentWithUse />
       </section>
 
       <section>
-        <h2 className="text-2xl font-bold">Client API fetching</h2>
+        <h2 className="text-2xl font-bold">Client fetching with use()</h2>
+        <p className="text-sm text-gray-600">
+          This apparently also becomes static on build?
+        </p>
+        <p className="text-sm text-gray-600">
+          It currently does not work with actual fetch() though, which will
+          cause infinite network calls
+        </p>
         <ClientAPIContent />
       </section>
 
       <section>
         <h2 className="text-2xl font-bold">Server fetch on request</h2>
 
+        <p className="text-sm text-gray-600">
+          This is also a server component like the first section, but the fetch
+          request is forced to be called per request instead of only on build,
+          making it the old gSSP effectively.
+        </p>
         <Link href="/ssr" className="text-blue-600 hover:text-blue-800">
           Go to page
         </Link>
@@ -36,6 +57,10 @@ export default function Home() {
 
       <section>
         <h2 className="text-2xl font-bold">Client side fetching with RQ</h2>
+        <p className="text-sm text-gray-600">
+          This is (actual) client side fetching with react query. The layout of
+          it is also a client component since it needs context.
+        </p>
         <Link
           href="/client-fetch"
           className="text-blue-600 hover:text-blue-800">
@@ -44,6 +69,26 @@ export default function Home() {
       </section>
       <section>
         <h2 className="text-2xl font-bold">Client side fetching with tRPC</h2>
+        <p className="text-sm text-gray-600">
+          This is also an (actual) client side fetching with react query. The
+          layout of it is also a client component since it needs context.
+        </p>
+        <p className="text-sm text-gray-600">
+          It is set up with the{" "}
+          <Link
+            href="https://trpc.io/docs/v10/react"
+            className="text-blue-600 hover:text-blue-800"
+            target="_blank">
+            Usage with React
+          </Link>{" "}
+          section in tRPC v10 docs instead of{" "}
+          <Link
+            href="https://trpc.io/docs/v10/nextjs"
+            className="text-blue-600 hover:text-blue-800"
+            target="_blank">
+            Usage with Next.js
+          </Link>
+        </p>
         <Link href="/trpc" className="text-blue-600 hover:text-blue-800">
           Go to page
         </Link>
