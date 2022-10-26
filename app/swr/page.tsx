eed0dbeb0b3e8d47ff7c387ddacbe5214496c052
Huy Anh Nguyen <26082352+bte234@users.noreply.github.com>
2022-10-26 16:57:11 -0700

    change swr api to jsonplaceholder

--- app/swr/page.tsx
+++ app/swr/page.tsx
@@ -5,14 +5,17 @@ import useSWR from "swr";
 const fetchData = (url: string) => fetch(url).then((res) => res.json());
 
 const SWRPage = () => {
-  const { data } = useSWR(`/api/hello`, fetchData);
+  const { data } = useSWR(
+    `https://jsonplaceholder.typicode.com/todos/1`,
+    fetchData
+  );
 
   if (!data) return <div>SWR loading...</div>;
   return (
     <div>
       <h1 className="text-4xl font-bold">SWR fetching</h1>
       <p>
-        data: <span className="text-red-600 font-semibold">{data.name}</span>
+        data: <span className="text-red-600 font-semibold">{data.title}</span>
       </p>
     </div>
   );
