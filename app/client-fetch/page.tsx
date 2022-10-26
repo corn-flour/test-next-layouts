d31348527ce298db6041eaa9444ff1135951c03d
Huy Anh Nguyen <26082352+bte234@users.noreply.github.com>
2022-10-25 23:51:32 -0700

    test: return <></> on server

--- app/client-fetch/page.tsx
+++ app/client-fetch/page.tsx
@@ -1,6 +1,7 @@
 import { RQFetch } from "../../components/client-rq";
 
 const ClientFetch = () => {
+  if (typeof window !== "undefined") return <></>;
   return (
     <main>
       <h1 className="text-4xl font-bold">Fetch with RQ</h1>
