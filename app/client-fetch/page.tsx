140fc6500a6283d120e40cb94631f8686605cac5
Huy Anh Nguyen <26082352+bte234@users.noreply.github.com>
2022-10-25 18:16:04 -0700

    rq

---
+++ app/client-fetch/page.tsx
@@ -0,0 +1,12 @@
+import { RQFetch } from "../../components/client-rq";
+
+const ClientFetch = () => {
+  return (
+    <main>
+      <h1 className="text-4xl font-bold">Fetch with RQ</h1>
+      <RQFetch />
+    </main>
+  );
+};
+
+export default ClientFetch;
---
+++ components/client-rq.tsx
@@ -0,0 +1,16 @@
+"use client";
+
+import { useQuery } from "@tanstack/react-query";
+
+const fetchData = async () =>
+  fetch("http://localhost:3000/api/hello", {
+    cache: "no-store",
+  }).then((res) => res.json());
+
+export const RQFetch = () => {
+  const { data } = useQuery(["fetchdata"], fetchData);
+
+  if (!data) return <div>waiting...</div>;
+
+  return <p>React Query response: {data.name}</p>;
+};
