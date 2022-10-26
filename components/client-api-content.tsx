f25c28f932a2138e532ba9e78763a01b5e4f0a72
Huy Anh Nguyen <26082352+bte234@users.noreply.github.com>
2022-10-25 17:38:26 -0700

    add example of client fetch makes infi api calls

--- components/client-api-content.tsx
+++ components/client-api-content.tsx
@@ -3,9 +3,14 @@
 import { use } from "react";
 import { getData } from "../service/api";
 
-// This in theory is a component that fetches from the API on the client side using the use() hook.
-// It currently might break with actual fetch
+// !If used with actual fetch, this component will break and cause an infinite number of network calls
 // https://beta.nextjs.org/docs/data-fetching/fetching#example-fetch-and-use-in-client-components
+// const fetchData = async () =>
+//   fetch("http://localhost:3000/api/hello", {
+//     cache: "no-store",
+//   }).then((res) => res.json());
+
+// This in theory is a component that fetches from the API on the client side using the use() hook.
 export const ClientAPIContent = () => {
   const data = use(getData()) as {
     name: string;
