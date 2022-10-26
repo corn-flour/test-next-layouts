70b12232dc454f6b033d6080722e19278a9ae9f9
Huy Anh Nguyen <26082352+bte234@users.noreply.github.com>
2022-10-25 23:48:36 -0700

    remove settimeout in /api/hello

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -10,7 +10,5 @@ export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  setTimeout(() => {
-    res.status(200).json({ name: "Hello from /api/hello" });
-  }, 1000);
+  return res.status(200).json({ name: "Hello from /api/hello" });
 }
